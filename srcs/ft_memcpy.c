fe1274bb6b2d17fdef122deecdffb5b44a3070ac
Louis Touzalin <ltouzali@3G4.42angouleme.fr>
2024-07-12 11:58:10 +0200

    improved memset and memcpy

--- srcs/ft_memcpy.c
+++ srcs/ft_memcpy.c
@@ -1,64 +1,58 @@
 #include "../libft.h"
 
-inline void *ft_memcpy(void *dst, const void *src, size_t n)
+/*
+ *  this implementation of the memcpy function using AVX2 instructions
+ *  less instruction, less cache misses, less branch mispredictions
+*/
+
+void *ft_memcpy(void *dst, const void *src, size_t n)
 {
     unsigned char *d = (unsigned char *)dst;
     const unsigned char *s = (const unsigned char *)src;
-    size_t dst_align = (uintptr_t)dst % 32;
+    size_t dst_align = (uintptr_t)dst & 31;
     size_t initial_bytes = 0;
-
-    // Align the destination to 32 bytes
+	/*
+		* handle the initial bytes separately if the pointer is not aligned on a 32 bytes boundary.
+		* load 32 bytes from the memory pointed by s and store them in the memory pointed by d.
+		* prefetch the next 32 bytes to improve performance.
+	*/
     if (dst_align != 0)
     {
         initial_bytes = 32 - dst_align;
         if (initial_bytes > n)
             initial_bytes = n;
         for (size_t i = 0; i < initial_bytes; i++)
+		{
             *d++ = *s++;
+			_mm_prefetch((const char *)(s), _MM_HINT_T0);
+		}
         n -= initial_bytes;
     }
 
-    // Prefetch source and destination
-    _mm_prefetch((const char *)(s), _MM_HINT_T0);
-    _mm_prefetch((const char *)(s + 64), _MM_HINT_T0);
-    _mm_prefetch((const char *)(d), _MM_HINT_T0);
-    _mm_prefetch((const char *)(d + 64), _MM_HINT_T0);
-
-    size_t chunks = n / 256; // 256-bit registers, hence 256 bytes per iteration
+	/*
+		* chunks is the number of 32 bytes chunks in the memory block.
+		* remaining_bytes is the number of bytes that are not part of a 32 bytes chunk.
+		* The function copies the memory 32 bytes at a time.
+		* The function uses prefetching to improve performance.
+	*/
+    size_t chunks = n / 256;
     size_t remaining_bytes = n % 256;
-
-    // Unrolled loop to handle 256 bytes per iteration
     for (size_t i = 0; i < chunks; i++)
     {
-        _mm_prefetch((const char *)(s + 256), _MM_HINT_T0);
-        _mm_prefetch((const char *)(d + 256), _MM_HINT_T0);
 
         __m256i chunk0 = _mm256_loadu_si256((__m256i *)(s + 0));
-        __m256i chunk1 = _mm256_loadu_si256((__m256i *)(s + 32));
-        __m256i chunk2 = _mm256_loadu_si256((__m256i *)(s + 64));
-        __m256i chunk3 = _mm256_loadu_si256((__m256i *)(s + 96));
-        __m256i chunk4 = _mm256_loadu_si256((__m256i *)(s + 128));
-        __m256i chunk5 = _mm256_loadu_si256((__m256i *)(s + 160));
-        __m256i chunk6 = _mm256_loadu_si256((__m256i *)(s + 192));
-        __m256i chunk7 = _mm256_loadu_si256((__m256i *)(s + 224));
-
         _mm256_storeu_si256((__m256i *)(d + 0), chunk0);
-        _mm256_storeu_si256((__m256i *)(d + 32), chunk1);
-        _mm256_storeu_si256((__m256i *)(d + 64), chunk2);
-        _mm256_storeu_si256((__m256i *)(d + 96), chunk3);
-        _mm256_storeu_si256((__m256i *)(d + 128), chunk4);
-        _mm256_storeu_si256((__m256i *)(d + 160), chunk5);
-        _mm256_storeu_si256((__m256i *)(d + 192), chunk6);
-        _mm256_storeu_si256((__m256i *)(d + 224), chunk7);
 
         s += 256;
         d += 256;
+        _mm_prefetch((const char *)(s + 256), _MM_HINT_T0);
+        _mm_prefetch((const char *)(d + 256), _MM_HINT_T0);
     }
-
-    // Copy the remaining bytes
-    for (size_t i = 0; i < remaining_bytes; i++)
+    for (size_t i = 0; i < initial_bytes; i++)
+    {
         *d++ = *s++;
+        _mm_prefetch((const char *)(s), _MM_HINT_T0);
+    }
 
     return dst;
 }
-
--- srcs/ft_memset.c
+++ srcs/ft_memset.c
@@ -1,42 +1,60 @@
 #include "../libft.h"
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
-#include <x86intrin.h>
-#include <immintrin.h>
-#include <stddef.h>
-#include <stdint.h>
 
 #define VEC_SIZE 32
 #define BLOCK_SIZE 4096
 
-void *ft_memset(void *b, int c, size_t len) 
+/*
+	* this implementation of the memset function uses AVX2 
+*	* instructions to set the memory pointed by b to the value c 
+*	* the function loads 32 bytes at a time and sets the memory to the value c
+*
+*	* Explanation:
+*	* The function uses AVX2 instructions to set the memory pointed by b to the value c.
+*	* The function loads 32 bytes at a time and sets the memory to the value c.
+*	* The function handles the initial bytes separately if the pointer is not aligned on a 32 bytes boundary.
+*	* The function uses prefetching to improve performance.
+*/
+
+inline void *ft_memset(void *b, int c, size_t len) 
 {
     unsigned char *ptr = (unsigned char *)b;
     unsigned char value = (unsigned char)c;
     __m256i v = _mm256_set1_epi8(value);
-
-    // Handle initial misaligned bytes
+	_mm_prefetch(ptr, _MM_HINT_NTA);
     size_t offset = (uintptr_t)ptr & (VEC_SIZE - 1);
-    if (offset != 0) 
+    if (offset) 
     {
         size_t align_size = VEC_SIZE - offset;
         align_size = (align_size > len) ? len : align_size;
 
         for (size_t i = 0; i < align_size; i++) {
             ptr[i] = value;
-        }
 
+		}
+			
         ptr += align_size;
         len -= align_size;
-    }
+		_mm_prefetch(ptr, _MM_HINT_NTA);
+		_mm_prefetch(ptr + VEC_SIZE, _MM_HINT_NTA);
+	}
+
+	/*
+		* chunks is the number of 32 bytes chunks in the memory block.
+		* remainings is the number of bytes that are not part of a 32 bytes chunk.
+		* The function sets the memory to the value c 32 bytes at a time.
+	*/
 
     size_t chunks = len / VEC_SIZE;
     size_t remainings = len % VEC_SIZE;
 
-    // Process chunks using AVX2
+	/*
+		* Prefetch the next 64 bytes to improve performance.
+		* The _MM_HINT_NTA hint is used to indicate that the data is not accessed again soon.
+		* This hint is useful when the data is not accessed sequentially.
+	*/
+	_mm_prefetch(ptr + chunks * VEC_SIZE, _MM_HINT_NTA);
     for (size_t i = 0; i < chunks; i++) {
+		_mm_prefetch(ptr + (i + 1) * VEC_SIZE, _MM_HINT_NTA);
         _mm256_storeu_si256((__m256i *)(ptr + i * VEC_SIZE), v);
     }
 
@@ -48,16 +66,5 @@ void *ft_memset(void *b, int c, size_t len)
     }
 
     return b;
-}
-
-int main(void)
-{
-    char str[100] = "Hello, World!";
-    ft_memset(str, 'A', 5);
-    char str2[100] = "Hello, World!";
-    memset(str2, 'A', 5);
-    printf("%s\n", str2);
-    printf("%s\n", str);
-    return 0;
 }
 
