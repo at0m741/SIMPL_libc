5a5b3c46a5435a742a714d39a9d8cf5246344afb
bonsthie <barnabe.bonnet@gmail.com>
2024-07-14 17:58:08 +0200

    clean

--- asm_out/ft_strdup.s
+++
@@ -1,6 +0,0 @@
-	.text
-	.intel_syntax noprefix
-	.file	"ft_strdup.c"
-	.ident	"Ubuntu clang version 12.0.1-19ubuntu3"
-	.section	".note.GNU-stack","",@progbits
-	.addrsig
--- asm_out/ft_strlen_sse.s
+++
@@ -1,6 +0,0 @@
-	.text
-	.intel_syntax noprefix
-	.file	"ft_strlen_sse.c"
-	.ident	"Ubuntu clang version 12.0.1-19ubuntu3"
-	.section	".note.GNU-stack","",@progbits
-	.addrsig
