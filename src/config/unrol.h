b42f83f1eab092c6070429c9f237a2138c3d5f72
bonsthie <barnabe.bonnet@gmail.com>
2024-07-24 23:25:40 +0200

    add of unroll

---
+++ src/config/unrol.h
@@ -0,0 +1,43 @@
+#ifndef __URNOLL_H_
+#define __URNOLL_H_
+
+#define UROLL_2(func)                                                          \
+  func;                                                                        \
+  func;
+
+#define UROLL_4(func)                                                          \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;
+
+#define UROLL_8(func)                                                          \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;
+
+#define UROLL_16(func)                                                         \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;                                                                        \
+  func;
+
+
+#endif // __URNOLL_H_
