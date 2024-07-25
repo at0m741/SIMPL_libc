bae9dba368993bc5fa487d4566ff1b00c8c104c5
Louis Touzalin <ltouzali@1G1.42angouleme.fr>
2024-07-25 23:34:58 +0200

    mmap, (yes, without any libs, just a syscall 0x9)

--- src/config/cpuid_conf.h
+++ src/config/cpuid_conf.h
@@ -3,23 +3,23 @@
 
 #include <stdint.h>
 
-#define Bit_MMX        (1 << 23)
-#define Bit_SSE        (1 << 25)
-#define Bit_SSE2       (1 << 26)
-#define Bit_SSE3       (1 << 0)
-#define Bit_SSSE3      (1 << 9)
-#define Bit_SSE4_1     (1 << 19)
-#define Bit_SSE4_2     (1 << 20)
-#define Bit_AVX        (1 << 28)
-#define Bit_AVX2       (1 << 5)
-#define Bit_AVX512F    (1 << 16)
-#define Bit_AVX512DQ   (1 << 17)
-#define Bit_AVX512IFMA (1 << 21)
-#define Bit_AVX512PF   (1 << 26)
-#define Bit_AVX512ER   (1 << 27)
-#define Bit_AVX512CD   (1 << 28)
-#define Bit_AVX512BW   (1 << 30)
-#define Bit_AVX512VL   (1 << 31)
+#define bit_MMX        (1 << 23)
+#define bit_SSE        (1 << 25)
+#define bit_SSE2       (1 << 26)
+#define bit_SSE3       (1 << 0)
+#define bit_SSSE3      (1 << 9)
+#define bit_SSE4_1     (1 << 19)
+#define bit_SSE4_2     (1 << 20)
+#define bit_AVX        (1 << 28)
+#define bit_AVX2       (1 << 5)
+#define bit_AVX512F    (1 << 16)
+#define bit_AVX512DQ   (1 << 17)
+#define bit_AVX512IFMA (1 << 21)
+#define bit_AVX512PF   (1 << 26)
+#define bit_AVX512ER   (1 << 27)
+#define bit_AVX512CD   (1 << 28)
+#define bit_AVX512BW   (1 << 30)
+#define bit_AVX512VL   (1 << 31)
 
 typedef union simd_support_u {
     uint32_t cpu_id;
@@ -41,9 +41,8 @@ typedef union simd_support_u {
         uint8_t avx512cd  : 1;
         uint8_t avx512bw  : 1;
         uint8_t avx512vl  : 1;
-        uint8_t erms	  : 1;
-
-		uint32_t padding  : 14;
+        uint8_t erms      : 1;
+        uint32_t padding  : 14;
     };
 } simd_support_t;
 
--- src/malloc/mmap.c
+++ src/malloc/mmap.c
@@ -1,3 +1,81 @@
-#include "malloc.h"
 #include <config.h>
+
+/* mmap syscall */
+/* 
+ * void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
+ * 
+ * addr: starting address of the memory region to be mapped
+ * length: length of the memory region to be mapped
+ * prot: protection flags
+ * flags: mapping flags
+ * fd: file descriptor
+ * offset: offset in the file
+ * 
+ * return value: starting address of the mapped memory region
+ * 
+ * This function maps a memory region of length length starting from addr.
+ * The memory region is mapped with the protection flags prot and mapping flags flags.
+ * If fd is not -1, the memory region is mapped from the file with file descriptor fd starting from offset.
+ * 
+ * If the function fails, it returns MAP_FAILED.
+ */
+
+#define PROT_READ 0x1
+#define PROT_WRITE 0x2
+#define PROT_EXEC 0x4
+
+#define MAP_PRIVATE 0x02
+#define MAP_ANONYMOUS 0x20
+#define MAP_FAILED ((void *)-1)
+
+#define MAP_FIXED 0x10
+
+
+void* _mmap(void* start, size_t len, int prot, int flags, int fd, size_t offset) {
+    if (!len || !prot) {
+        return MAP_FAILED;
+    }
+
+	/* check if the start address is NULL */
+	/* if it is, set the start address to 0 */
+	if (!start) {
+		start = 0;
+	}
+	/* these registers are used to pass the arguments to the syscall */
+
+    void* result;
+    register uint64_t r10 asm("r10") = flags;
+    register uint64_t r8 asm("r8") = fd;
+    register uint64_t r9 asm("r9") = offset;
+
+	/* make mmap syscall */
+	/* rax = 0x9, syscall number of mmap */
+	/* rdi = start, starting address of the memory region to be mapped */
+	/* rsi = len, length of the memory region to be mapped */
+	/* rdx = prot, protection flags */
+	/* r10 = flags, mapping flags */
+	/* r8 = fd, file descriptor */
+	/* r9 = offset, offset in the file */
+	/* syscall, result is stored in rax */
+    __asm__ __volatile__ (
+        ".intel_syntax noprefix\n"
+        "mov rax, 0x9\n"                   
+        "syscall\n"
+	  : "=a"(result)           
+	  : "D"(start)	, "S"(len),
+	    "d"(prot)	, "r"(r10), 
+     	"r"(r8)		, "r"(r9)
+      : "memory", 
+		"rcx", 
+		"r11"
+    );
+
+	/* check if the result is negative */
+	/* if it is, return MAP_FAILED */
+    if ((uint64_t)result < 0) {
+        return MAP_FAILED;
+    }
+
+    return result;
+}
 
