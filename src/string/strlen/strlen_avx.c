8f8a52bf2c1f7875666d543fd3e332719cf82be9
Louis Touzalin <ltouzali@3D3.42angouleme.fr>
2024-09-28 17:14:29 +0200

    finaly a noice strlen in C

--- src/string/strlen/strlen_avx.c
+++ src/string/strlen/strlen_avx.c
@@ -18,63 +18,53 @@ __builtin_ctz() is a builtin function that returns the number of trailing 0-bits
 #include <immintrin.h>
 
 
-size_t _strlen_avx(const char *str) {
+uint64_t strlen_avx(const char *str) 
+{
     if (__builtin_expect(str == NULL, 0))
-        return 0;
+		return 0;
 
-    const char *original_ptr = str;
-    __m256i ymm_zero = _mm256_setzero_si256();
+    const char *original_ptr = str;  
+    __m256i ymm_zero = _mm256_set1_epi8(0);
 
     uintptr_t misalignment = (uintptr_t)str & 31;
-    if (misalignment != 0) {
-        uintptr_t offset = 32 - misalignment;
-        __m256i ymm_data = _mm256_loadu_si256((const __m256i *)str);
-        int32_t mask = _mm256_movemask_epi8(_mm256_cmpeq_epi8(ymm_data, ymm_zero));
+    if (misalignment != 0) 
+    {
+        int64_t offset = 32 - misalignment;
+        __m256i ymm_data = _mm256_loadu_si256((__m256i*)str);
+        __m256i cmp_result = _mm256_cmpeq_epi8(ymm_zero, ymm_data);
+        int32_t mask = _mm256_movemask_epi8(cmp_result);
 
-        if (mask != 0) {
+		mask >>= misalignment;
+
+        if (mask != 0) 
+        {
             int32_t index = __builtin_ctz(mask);
-            return (str + index) - original_ptr;
+			_mm256_zeroupper();
+            return (uintptr_t)(str + index) - (uintptr_t)original_ptr;
         }
+
         str += offset;
     }
-
-    while (1) {
-        _mm_prefetch((const char *)(str + 256), _MM_HINT_T0);
-
-        __m256i ymm_data1 = _mm256_load_si256((const __m256i *)(str));
-        __m256i ymm_data2 = _mm256_load_si256((const __m256i *)(str + 32));
-        __m256i ymm_data3 = _mm256_load_si256((const __m256i *)(str + 64));
-        __m256i ymm_data4 = _mm256_load_si256((const __m256i *)(str + 96));
-
-        __m256i cmp_result1 = _mm256_cmpeq_epi8(ymm_data1, ymm_zero);
-        __m256i cmp_result2 = _mm256_cmpeq_epi8(ymm_data2, ymm_zero);
-        __m256i cmp_result3 = _mm256_cmpeq_epi8(ymm_data3, ymm_zero);
-        __m256i cmp_result4 = _mm256_cmpeq_epi8(ymm_data4, ymm_zero);
-
-        int32_t mask1 = _mm256_movemask_epi8(cmp_result1);
-        int32_t mask2 = _mm256_movemask_epi8(cmp_result2);
-        int32_t mask3 = _mm256_movemask_epi8(cmp_result3);
-        int32_t mask4 = _mm256_movemask_epi8(cmp_result4);
-
-        if (mask1 != 0) {
-            int32_t index = __builtin_ctz(mask1);
-            return (str + index) - original_ptr;
-        }
-        if (mask2 != 0) {
-            int32_t index = __builtin_ctz(mask2);
-            return (str + 32 + index) - original_ptr;
-        }
-        if (mask3 != 0) {
-            int32_t index = __builtin_ctz(mask3);
-            return (str + 64 + index) - original_ptr;
-        }
-        if (mask4 != 0) {
-            int32_t index = __builtin_ctz(mask4);
-            return (str + 96 + index) - original_ptr;
+	uintptr_t page_offset = (uintptr_t)str & 31;
+    while (1) 
+    {
+        __m256i ymm_data = _mm256_load_si256((__m256i*)str);  
+        __m256i cmp_result = _mm256_cmpeq_epi8(ymm_zero, ymm_data);
+        int32_t mask = _mm256_movemask_epi8(cmp_result);
+		
+		mask >>= page_offset;
+        if (mask != 0) 
+        {
+            int32_t index = __builtin_ctz(mask);
+			_mm256_zeroupper();
+            return (uintptr_t)(str + index) - (uintptr_t)original_ptr;
         }
 
-        str += 128; 
+        str += 32;
     }
+	_mm256_zeroupper();
+
+    return 0;
 }
 
 /*  */
