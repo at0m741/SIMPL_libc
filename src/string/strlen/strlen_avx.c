281312224b95a24d674d45725466ba744111e62c
at0m741 <louistouz@icloud.com>
2024-10-12 23:03:29 +0200

    Updated strlen_avx

--- src/string/strlen/strlen_avx.c
+++ src/string/strlen/strlen_avx.c
@@ -18,60 +18,45 @@ __builtin_ctz() is a builtin function that returns the number of trailing 0-bits
 #include <immintrin.h>
 
 
-uint64_t _strlen_avx(const char *str) 
+size_t _strlen_avx(const char *str) 
 {
-    if (__builtin_expect(str == NULL, 0))
-		return 0;
-
     const char *original_ptr = str;  
     __m256i ymm_zero = _mm256_set1_epi8(0);
 
     uintptr_t misalignment = (uintptr_t)str & 31;
     if (misalignment != 0) 
     {
-        int64_t offset = 32 - misalignment;
+        size_t offset = 32 - misalignment;
         __m256i ymm_data = _mm256_loadu_si256((__m256i*)str);
         __m256i cmp_result = _mm256_cmpeq_epi8(ymm_zero, ymm_data);
         int32_t mask = _mm256_movemask_epi8(cmp_result);
 
-		mask >>= misalignment;
+        mask >>= misalignment;
 
         if (mask != 0) 
         {
             int32_t index = __builtin_ctz(mask);
-			_mm256_zeroupper();
-            return (uintptr_t)(str + index) - (uintptr_t)original_ptr;
+            return (size_t)(str + index - original_ptr);
         }
 
         str += offset;
     }
-	uintptr_t page_offset = (uintptr_t)str & 31;
+
     while (1) 
     {
+	_mm_prefetch(str + 32, _MM_HINT_T0);
         __m256i ymm_data = _mm256_load_si256((__m256i*)str);  
         __m256i cmp_result = _mm256_cmpeq_epi8(ymm_zero, ymm_data);
         int32_t mask = _mm256_movemask_epi8(cmp_result);
-		
-		mask >>= page_offset;
+
         if (mask != 0) 
         {
             int32_t index = __builtin_ctz(mask);
-			_mm256_zeroupper();
-            return (uintptr_t)(str + index) - (uintptr_t)original_ptr;
+            return (size_t)(str + index - original_ptr);
         }
 
         str += 32;
     }
-	_mm256_zeroupper();
-
     return 0;
 }
 
-/*  */
-/* void test (){ */
-/*  */
-/* 	char *s; */
-/* 	char *d; */
-/* 	UNROLL_4(_mm256_storeu_si256(d++, _mm256_loadu_si256(s++))) */
-/* 	 */
-/* } */
