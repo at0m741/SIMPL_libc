5db3afea6cebc155353555327de1aff4fe7f4731
at0m741 <louistouz@icloud.com>
2024-10-12 23:06:44 +0200

    Updated memmove_avx

--- src/string/memmove/memmove_avx.c
+++ src/string/memmove/memmove_avx.c
@@ -15,82 +15,49 @@
  * The function uses prefetching to improve performance.
 */
 
-#define PREFETCH_SIZE 64
-#define LARGE_THRESHOLD 524288 
-#define VEC_SIZE 32
-
-void *_memmove_avx(void *dest, const void *src, size_t n) 
-{
-    if (dest == NULL || src == NULL || n == 0)
+void *_memmove_avx(void *dest, const void *src, size_t len) {
+    if (len == 0 || dest == src)
         return dest;
 
-    char *destp = (char *)dest;
-    const char *srcp = (const char *)src;
+    uint8_t *cdest = (uint8_t *)dest;
+    const uint8_t *csrc = (const uint8_t *)src;
 
-    size_t misalignment = ((uintptr_t)destp % VEC_SIZE);
-    if (misalignment != 0) {
-        size_t adjust = VEC_SIZE - misalignment;
-        if (adjust > n) adjust = n;
-        while (adjust--) {
-            *destp++ = *srcp++;
-            n--;
-        }
-    }
+    if (cdest < csrc) {
+        size_t num_blocks = len / 32;
+        size_t remaining_bytes = len % 32;
 
-    size_t num_avx_iterations = n / VEC_SIZE;
-    size_t remainder = n % VEC_SIZE;
+        const __m256i *s = (const __m256i *)csrc;
+        __m256i *d = (__m256i *)cdest;
+		
+        for (size_t i = 0; i < num_blocks; ++i)
+            _mm256_storeu_si256(d++, _mm256_loadu_si256(s++));
 
-    if (n >= LARGE_THRESHOLD) {
-        for (size_t i = 0; i < n; i += PREFETCH_SIZE)
-            _mm_prefetch(srcp + i, _MM_HINT_T0);
-
-        while (num_avx_iterations >= 8) {
-            __m256i d0 = _mm256_stream_load_si256((const __m256i *)srcp);
-            __m256i d1 = _mm256_stream_load_si256((const __m256i *)(srcp + VEC_SIZE));
-            __m256i d2 = _mm256_stream_load_si256((const __m256i *)(srcp + 2 * VEC_SIZE));
-            __m256i d3 = _mm256_stream_load_si256((const __m256i *)(srcp + 3 * VEC_SIZE));
-            __m256i d4 = _mm256_stream_load_si256((const __m256i *)(srcp + 4 * VEC_SIZE));
-            __m256i d5 = _mm256_stream_load_si256((const __m256i *)(srcp + 5 * VEC_SIZE));
-            __m256i d6 = _mm256_stream_load_si256((const __m256i *)(srcp + 6 * VEC_SIZE));
-            __m256i d7 = _mm256_stream_load_si256((const __m256i *)(srcp + 7 * VEC_SIZE));
-            
-            _mm256_stream_si256((__m256i *)destp, d0);
-            _mm256_stream_si256((__m256i *)(destp + VEC_SIZE), d1);
-            _mm256_stream_si256((__m256i *)(destp + 2 * VEC_SIZE), d2);
-            _mm256_stream_si256((__m256i *)(destp + 3 * VEC_SIZE), d3);
-            _mm256_stream_si256((__m256i *)(destp + 4 * VEC_SIZE), d4);
-            _mm256_stream_si256((__m256i *)(destp + 5 * VEC_SIZE), d5);
-            _mm256_stream_si256((__m256i *)(destp + 6 * VEC_SIZE), d6);
-            _mm256_stream_si256((__m256i *)(destp + 7 * VEC_SIZE), d7);
-
-            srcp += 8 * VEC_SIZE;
-            destp += 8 * VEC_SIZE;
-            num_avx_iterations -= 8;
-            _mm_prefetch(srcp + (8 * VEC_SIZE), _MM_HINT_T0);
-        }
-
-        _mm_sfence();
+        uint8_t *byte_dest = (uint8_t *)d;
+        const uint8_t *byte_src = (const uint8_t *)s;
+        for (size_t i = 0; i < remaining_bytes; ++i) 
+            byte_dest[i] = byte_src[i];
     } else {
-        while (num_avx_iterations--) {
-            __m256i data = _mm256_loadu_si256((const __m256i *)srcp);
-            _mm256_storeu_si256((__m256i *)destp, data);
-            srcp += VEC_SIZE;
-            destp += VEC_SIZE;
+        size_t num_blocks = len / 32;
+        size_t remaining_bytes = len % 32;
+
+        const __m256i *s = (const __m256i *)(csrc + len);
+        __m256i *d = (__m256i *)(cdest + len);
+
+        if (remaining_bytes) {
+            const uint8_t *byte_src = csrc + len;
+            uint8_t *byte_dest = cdest + len;
+            for (size_t i = 0; i < remaining_bytes; ++i)
+                byte_dest[-(int)(i + 1)] = byte_src[-(int)(i + 1)];
+            s = (const __m256i *)(csrc + len - remaining_bytes);
+            d = (__m256i *)(cdest + len - remaining_bytes);
+        }
+
+        for (size_t i = 0; i < num_blocks; ++i) {
+            d--;
+            s--;
+            _mm256_storeu_si256(d, _mm256_loadu_si256(s));
         }
     }
 
-    if (remainder > 0) {
-        if (remainder >= 16) {
-			_mm_prefetch(srcp + 16, _MM_HINT_T0);
-            __m128i data = _mm_loadu_si128((const __m128i *)srcp);
-            _mm_storeu_si128((__m128i *)destp, data);
-            srcp += 16;
-            destp += 16;
-            remainder -= 16;
-        }
-        while (remainder--)
-            *destp++ = *srcp++;
-    }
-	_mm256_zeroupper();
     return dest;
 }
